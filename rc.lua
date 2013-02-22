ec404f86ce08e468e1cefb4ea3994f4fafd315d9
Max <silverhammermba@gmail.com>
2013-02-22 13:11:07 -0500

    Readd rule for floating wicd dialog

--- rc.lua
+++ rc.lua
@@ -332,6 +332,8 @@ awful.rules.rules = {
                      buttons = clientbuttons } },
     { rule = { class = "pinentry" },
       properties = { floating = true } },
+    { rule = { name = "Wicd Network Manager" },
+      properties = { floating = true } },
 }
 -- }}}
 
