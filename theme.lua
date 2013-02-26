de0ee02fdc9d3154af6de87ccf8a90e43f144f76
Max <silverhammermba@gmail.com>
2013-02-25 19:49:00 -0500

    Change awesome colors (pastels!)

--- theme.lua
+++ theme.lua
@@ -4,15 +4,15 @@
 
 theme = {}
 
-theme.font          = "sans 8"
+theme.font          = "DejaVuSans 8"
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
+theme.bg_normal     = "#c7b7cb"
+theme.bg_focus      = "#995fa4"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
 
-theme.fg_normal     = "#aaaaaa"
+theme.fg_normal     = "#222222"
 theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
