4e883ba7dc8be958260b0795a0ce834db2d5cc02
Max <silverhammermba@gmail.com>
2014-08-16 12:27:27 -0400

    Change wallpaper

--- theme.lua
+++ theme.lua
@@ -73,7 +73,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/home/max/.config/awesome/the
 theme.titlebar_maximized_button_normal_active = "/home/max/.config/awesome/themes/max/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/home/max/.config/awesome/themes/max/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/home/max/.config/awesome/themes/max/background.png"
+theme.wallpaper = "/home/max/.config/awesome/themes/max/background.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/home/max/.config/awesome/themes/max/layouts/fairhw.png"
