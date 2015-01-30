8d952fc1734284be31744b3af51117c798fef1f5
Max <silverhammermba@gmail.com>
2015-01-30 09:37:27 -0500

    Latest changes from default awesome theme

--- awesome/.config/awesome/theme/theme.lua
+++ awesome/.config/awesome/theme/theme.lua
@@ -25,7 +25,8 @@ theme.border_marked = "#91231c"
 -- There are other variable sets
 -- overriding the default one when
 -- defined, the sets are:
--- [taglist|tasklist]_[bg|fg]_[focus|urgent]
+-- taglist_[bg|fg]_[focus|urgent|occupied|empty]
+-- tasklist_[bg|fg]_[focus|urgent]
 -- titlebar_[bg|fg]_[normal|focus]
 -- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
 -- mouse_finder_[color|timeout|animate_timeout|radius|factor]
@@ -90,8 +91,8 @@ theme.layout_dwindle = "/home/max/.config/awesome/theme/layouts/dwindlew.png"
 
 theme.awesome_icon = "/home/max/.config/awesome/theme/awesome.png"
 
--- Define the icon theme for application icons. If not set then the icons from
--- /usr/share/icons and /usr/share/icons/hicolor will be used.
+-- Define the icon theme for application icons. If not set then the icons 
+-- from /usr/share/icons and /usr/share/icons/hicolor will be used.
 theme.icon_theme = nil
 
 return theme
