15677039f51722850c4b935f766c1974e1b0448c
Max <silverhammermba@gmail.com>
2015-02-01 00:47:14 -0500

    Simplify paths, reorganize

--- awesome/.config/awesome/theme/theme.lua
+++ awesome/.config/awesome/theme/theme.lua
@@ -6,6 +6,8 @@ theme = {}
 
 theme.font          = "DejaVuSans 8"
 
+theme.wallpaper = ".config/awesome/theme/background.png"
+
 theme.bg_normal     = "#e03f83"
 theme.bg_focus      = "#6f3579"
 theme.bg_urgent     = "#ff0000"
@@ -24,34 +26,30 @@ theme.border_focus  = "#000000"
 theme.border_marked = "#000000"
 
 -- Display the taglist squares
-theme.taglist_squares_sel   = "/home/max/.config/awesome/theme/taglist/squarefw.png"
-theme.taglist_squares_unsel = "/home/max/.config/awesome/theme/taglist/squarew.png"
+theme.taglist_squares_sel   = ".config/awesome/theme/taglist/squares_sel.png"
+theme.taglist_squares_unsel = ".config/awesome/theme/taglist/squares_unsel.png"
 
 -- Variables set for theming the menu:
-theme.menu_submenu_icon = "/home/max/.config/awesome/theme/submenu.png"
-theme.menu_height = 15
-theme.menu_width  = 100
-
-theme.wallpaper = "/home/max/.config/awesome/theme/background.png"
+theme.awesome_icon      = ".config/awesome/theme/menu/awesome.png"
+theme.menu_submenu_icon = ".config/awesome/theme/menu/submenu.png"
+theme.menu_height       = 15
+theme.menu_width        = 100
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = "/home/max/.config/awesome/theme/layouts/fairhw.png"
-theme.layout_fairv = "/home/max/.config/awesome/theme/layouts/fairvw.png"
-theme.layout_floating  = "/home/max/.config/awesome/theme/layouts/floatingw.png"
-theme.layout_magnifier = "/home/max/.config/awesome/theme/layouts/magnifierw.png"
-theme.layout_max = "/home/max/.config/awesome/theme/layouts/maxw.png"
-theme.layout_fullscreen = "/home/max/.config/awesome/theme/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/home/max/.config/awesome/theme/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/home/max/.config/awesome/theme/layouts/tileleftw.png"
-theme.layout_tile = "/home/max/.config/awesome/theme/layouts/tilew.png"
-theme.layout_tiletop = "/home/max/.config/awesome/theme/layouts/tiletopw.png"
-theme.layout_spiral  = "/home/max/.config/awesome/theme/layouts/spiralw.png"
-theme.layout_dwindle = "/home/max/.config/awesome/theme/layouts/dwindlew.png"
+theme.layout_fairh      = ".config/awesome/theme/layouts/fairh.png"
+theme.layout_fairv      = ".config/awesome/theme/layouts/fairv.png"
+theme.layout_floating   = ".config/awesome/theme/layouts/floating.png"
+theme.layout_magnifier  = ".config/awesome/theme/layouts/magnifier.png"
+theme.layout_max        = ".config/awesome/theme/layouts/max.png"
+theme.layout_fullscreen = ".config/awesome/theme/layouts/fullscreen.png"
+theme.layout_tilebottom = ".config/awesome/theme/layouts/tilebottom.png"
+theme.layout_tileleft   = ".config/awesome/theme/layouts/tileleft.png"
+theme.layout_tile       = ".config/awesome/theme/layouts/tile.png"
+theme.layout_tiletop    = ".config/awesome/theme/layouts/tiletop.png"
+theme.layout_spiral     = ".config/awesome/theme/layouts/spiral.png"
+theme.layout_dwindle    = ".config/awesome/theme/layouts/dwindle.png"
 
-theme.awesome_icon = "/home/max/.config/awesome/theme/awesome.png"
-
--- Define the icon theme for application icons. If not set then the icons 
--- from /usr/share/icons and /usr/share/icons/hicolor will be used.
+-- default icon theme
 theme.icon_theme = nil
 
 return theme
